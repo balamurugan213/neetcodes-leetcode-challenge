f5c860f6ec5dd0116e45f2e85a3fbf0fd40081db
balamurugan213 <balainfo21@gmail.com>
2023-05-09 15:48:50 +0530

    still more changes

--- neetcode-challenge/next.config.js
+++ neetcode-challenge/next.config.js
@@ -1,14 +1,31 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
-    loader: 'akamai',
-    path: '',
+    loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
   },
-  assetPrefix: './',
 }
 
 
 module.exports = nextConfig
+
+
